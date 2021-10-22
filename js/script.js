7c807032a9d33765246900cf34a8f700acf85f3a
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-10-22 14:48:53 +0100

    clickable reading topics

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".jav").click(function(){
+        alert("this is what javascript is all about");
+    });
+    $(".ope").click(function(){
+ window.location.href="java.html";
+    });
+});
