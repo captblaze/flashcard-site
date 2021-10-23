aca59afcc03cc442c6011a8331d37a445437fd83
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-10-23 07:13:10 +0100

    operators

--- js/script.js
+++ js/script.js
@@ -1,8 +1,84 @@
 $(document).ready(function(){
     $(".jav").click(function(){
-        alert("this is what javascript is all about");
+        window.location.href="java.html";
     });
     $(".ope").click(function(){
- window.location.href="java.html";
+ window.location.href="operator.html";
     });
+    $(".var").click(function(){
+        window.location.href="variable.html";
+           });
+
+           $(".varName").click(function(){
+            window.location.href="variableNames.html";
+               });
+               $(".func").click(function(){
+                window.location.href="function.html";
+                   });
+
+               $(".meth").click(function(){
+                window.location.href="method.html";
+                   });
+
+ $(".arg").click(function(){
+        window.location.href="argument.html";
+              });
+
+        $(".par").click(function(){
+              window.location.href="parameter.html";
+                   });
+
+               $(".ret").click(function(){
+                  window.location.href="return.html";
+                     });
+
+                               $(".chain").click(function(){
+                                window.location.href="chainingmethod.html";
+                                   });
+
+                                   $(".str").click(function(){
+                                    window.location.href="string.html";
+                                       });
+
+                                       $(".boo").click(function(){
+                                        window.location.href="booleans.html";
+                                           });
+
+                                           $(".und").click(function(){
+                                            window.location.href="undefined.html";
+                                               });
+
+                                               $(".nan").click(function(){
+                                                window.location.href="nan.html";
+                                                   });
+
+                                                   $(".null").click(function(){
+                                                    window.location.href="null.html";
+                                                       });
+
+                                                       $(".alt").click(function(){
+                                                        window.location.href="alert.html";
+                                                           });
+
+                                                           $(".com").click(function(){
+                                                            window.location.href="comment.html";
+                                                               });
+
+                                                               $(".jqu").click(function(){
+                                                                window.location.href="jquery.html";
+                                                                   });
+
+                                                                   $(".attr").click(function(){
+                                                                    window.location.href="attribute.html";
+                                                                       });
+
+                                                                       $(".syn").click(function(){
+                                                                        window.location.href="syntax.html";
+                                                                           });
+
+                                                                           
+
+
+
+
 });
