ca669b3b3bf7115157b48cd2216c1122722058cd
Mgbanwune Onyekachi <standbymii@yahoo.com>
2021-10-25 17:14:16 +0100

    flashcard site

--- js/script.js
+++ js/script.js
@@ -9,74 +9,78 @@ $(document).ready(function(){
         window.location.href="variable.html";
            });
 
-           $(".varName").click(function(){
-            window.location.href="variableNames.html";
-               });
-               $(".func").click(function(){
-                window.location.href="function.html";
-                   });
+$(".varName").click(function(){
+window.location.href="variableNames.html";
+});
 
-               $(".meth").click(function(){
-                window.location.href="method.html";
-                   });
+$(".func").click(function(){
+window.location.href="function.html";
+});
+
+$(".meth").click(function(){
+window.location.href="method.html";
+});
 
  $(".arg").click(function(){
-        window.location.href="argument.html";
-              });
+window.location.href="argument.html";
+});
 
-        $(".par").click(function(){
-              window.location.href="parameter.html";
-                   });
+$(".par").click(function(){
+window.location.href="paramter.html";
+});
 
-               $(".ret").click(function(){
-                  window.location.href="return.html";
-                     });
+$(".ret").click(function(){
+window.location.href="return.html";
+});
 
-                               $(".chain").click(function(){
-                                window.location.href="chainingmethod.html";
-                                   });
+$(".chain").click(function(){
+window.location.href="chainingmethod.html";
+});
 
-                                   $(".str").click(function(){
-                                    window.location.href="string.html";
-                                       });
+$(".str").click(function(){
+window.location.href="string.html";
+});
 
-                                       $(".boo").click(function(){
-                                        window.location.href="booleans.html";
-                                           });
+$(".boo").click(function(){
+window.location.href="booleans.html";
+});
 
-                                           $(".und").click(function(){
-                                            window.location.href="undefined.html";
-                                               });
+$(".und").click(function(){
+window.location.href="undefined.html";
+});
 
-                                               $(".nan").click(function(){
-                                                window.location.href="nan.html";
-                                                   });
+$(".nan").click(function(){
+window.location.href="nan.html";
+});
 
-                                                   $(".null").click(function(){
-                                                    window.location.href="null.html";
-                                                       });
+$(".null").click(function(){
+window.location.href="null.html";
+});
 
-                                                       $(".alt").click(function(){
-                                                        window.location.href="alert.html";
-                                                           });
+$(".alt").click(function(){
+window.location.href="alert.html";
+});
 
-                                                           $(".com").click(function(){
-                                                            window.location.href="comment.html";
-                                                               });
+$(".com").click(function(){
+window.location.href="comment.html";
+});
 
-                                                               $(".jqu").click(function(){
-                                                                window.location.href="jquery.html";
-                                                                   });
+$(".jqu").click(function(){
+window.location.href="jquery.html";
+});
 
-                                                                   $(".attr").click(function(){
-                                                                    window.location.href="attribute.html";
-                                                                       });
+$(".attr").click(function(){
+window.location.href="attribute.html";
+});
 
-                                                                       $(".syn").click(function(){
-                                                                        window.location.href="syntax.html";
-                                                                           });
+$(".syn").click(function(){
+     window.location.href="syntax.html";
+});
 
-                                                                           
+$("button").click(function(){
+    window.location.href="index.html";
+});
+                                                                  
 
 
 
